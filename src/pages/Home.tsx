8ae70b0457861aa134e7352737add4346d7d857f
Adnan <s.adnan@aabtools.com>
2024-04-07 10:17:48 +0400

    Added 'GridStack' component to cleanup HomePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import Grid from '@mui/material/Unstable_Grid2'
-import Stack from '@mui/material/Stack'
+
 import product from '../assets/product'
 import Carousel from '../components/Carousel'
 import Features from '../components/Features'
 import Specifications from '../components/Specifications'
 import ItemsIncluded from '../components/ItemsIncluded'
-import Price from '../components/Price'
+import Prices from '../components/Prices'
 import Title from '../components/Title'
+import GridStack from '../ui/GridStack'
 
 type Props = {}
 
@@ -19,31 +20,25 @@ function Home({}: Props) {
     features,
     specification,
     price,
+    whats_included,
   } = product
 
   return (
     <Grid container spacing={2} py={2}>
-      <Grid xs={12}>
+      <GridStack>
         <Title model={model} brand={brand} searchTitle={searchTitle} />
-      </Grid>
-
-      <Grid xs={12}>
         <Carousel images={s3Images} />
-      </Grid>
+      </GridStack>
 
-      <Grid xs={8}>
-        <Stack spacing={2}>
-          <Features features={features} />
-          <Specifications specification={specification} />
-        </Stack>
-      </Grid>
+      <GridStack md={8}>
+        <Features features={features} />
+        <Specifications specification={specification} />
+      </GridStack>
 
-      <Grid xs={4} spacing={2}>
-        <Stack spacing={2}>
-          <Price price={price} />
-          <ItemsIncluded items={product.whats_included} />
-        </Stack>
-      </Grid>
+      <GridStack md={4}>
+        <Prices price={price} />
+        <ItemsIncluded items={whats_included} />
+      </GridStack>
     </Grid>
   )
 }
---
+++ src/ui/GridStack.tsx
@@ -0,0 +1,22 @@
+import Grid from '@mui/material/Unstable_Grid2'
+import Stack from '@mui/material/Stack'
+
+type Props = {
+  xs?: number
+  sm?: number
+  md?: number
+  lg?: number
+  xl?: number
+  spacing?: number
+  children?: any
+}
+
+function GridStack({ xs, sm, md, lg, xl, spacing, children }: Props) {
+  return (
+    <Grid xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+      <Stack spacing={spacing}>{children}</Stack>
+    </Grid>
+  )
+}
+
+export default GridStack
