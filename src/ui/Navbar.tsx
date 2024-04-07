9437db9935a25b49b5d58d2970913a9daed378f6
Adnan <s.adnan@aabtools.com>
2024-04-07 08:54:24 +0400

    Updated Navbar to MUI component

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,26 +1,23 @@
-import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
 import LightModeIcon from '@mui/icons-material/LightMode'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
 
 type Props = {}
 
 function Navbar({}: Props) {
   return (
-    <Box
-      bgcolor="teal"
-      display="flex"
-      alignItems="center"
-      justifyContent="space-between"
-      p={1}
-    >
-      <Typography variant="h5" fontWeight={700}>
-        Navbar
-      </Typography>
-      <IconButton>
-        <LightModeIcon />
-      </IconButton>
-    </Box>
+    <AppBar position="sticky">
+      <Toolbar variant="dense" sx={{ justifyContent: 'space-between' }}>
+        <Typography variant="h5" fontWeight={700}>
+          Navbar
+        </Typography>
+        <IconButton edge="end">
+          <LightModeIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   )
 }
 
