1f41bfbfb1f0f1a3ef9fc3c42a0b300def102699
Adnan <s.adnan@aabtools.com>
2024-04-15 20:41:38 +0400

    Fix for 'styled_default is not a function'

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
+  optimizeDeps: {
+    include: ['@mui/material/Unstable_Grid2'],
+  },
 })
