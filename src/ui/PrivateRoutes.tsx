1fe0f19111a190b7dec02d51e51481d8d1e390af
Adnan <s.adnan@aabtools.com>
2024-05-29 23:52:49 +0400

    Added private routes

--- src/ui/PrivateRoutes.tsx
+++ src/ui/PrivateRoutes.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
+import { useAuth } from '@/contexts/AuthContext'
 
 type Props = {
   children?: React.ReactNode
 }
 
 function PrivateRoutes({}: Props) {
-  let auth = false
-  return auth ? <Outlet /> : <Navigate to="/login" />
+  const { user } = useAuth()
+  return user ? <Outlet /> : <Navigate to="/login" />
 }
 
 export default PrivateRoutes
